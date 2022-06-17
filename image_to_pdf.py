54ba31b0dd59370bb9d823a4d1ab3957f084e383
cespinosa <fractaliusfciencias@gmail.com>
2022-06-16 21:21:30 -0500

    automatizacion completada

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -10,12 +10,13 @@ def svg_to_doc(path):
 def main():
     layers_number = 2
     doc = fitz.open()
-    page = doc.new_page()
-    xref = doc.add_ocg('diapo_1', on=True)
-    layer = svg_to_doc('presentacion/diapo_1.svg')
-    bb = layer[0].rect
-    page.set_mediabox(bb)
-    page.show_pdf_page(bb, layer, 0, oc=xref)
+    for n in range(layers_number):
+        page = doc.new_page()
+        xref = doc.add_ocg(f'diapo_{n+1}', on=True)
+        layer = svg_to_doc(f'presentacion/diapo_{n+1}.svg')
+        bb = layer[0].rect
+        page.set_mediabox(bb)
+        page.show_pdf_page(bb, layer, 0, oc=xref)
     doc.save("output.pdf")
 
 if __name__ == "__main__":
